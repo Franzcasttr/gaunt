24af7c75351645e48824315f9f1bd91e366a70bb
Franzcasttr <franciscastro.216.fc@gmail.com>
2023-06-20 17:50:03 +0800

    update latency

--- performance.js
+++ performance.js
@@ -6,7 +6,7 @@ export const options = {
 
   thresholds: {
     http_req_failed: ['rate<0.01'],
-    http_req_duration: ['p(95)<500'],
+    http_req_duration: ['p(95)<50'],
   },
 };
 export default function () {
