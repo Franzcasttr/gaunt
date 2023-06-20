67175299a147bffe147e5613a5414bbb3efebfb6
Franzcasttr <franciscastro.216.fc@gmail.com>
2023-06-20 16:52:31 +0800

    add testing check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Hello World!</h1>
       <button disabled={true} className='p-4 rounded-lg bg-blue-400'>
-        Click me
+        Click
       </button>
     </main>
   );
