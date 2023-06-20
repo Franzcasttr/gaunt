41bd71316875532c32540c1cab22765d426e2702
Franzcasttr <franciscastro.216.fc@gmail.com>
2023-06-20 15:58:03 +0800

    test sucess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Hello World!</h1>
-      <button disabled={false} className='p-4 rounded-lg bg-blue-400'>
+      <button disabled={true} className='p-4 rounded-lg bg-blue-400'>
         Click me
       </button>
     </main>
